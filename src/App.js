a244a629602be1f9c332b7d8fcb49ba2526b068a
Held Grijo <held.grijo@gmail.com>
2021-01-16 12:26:14 -0400

    Listing repos from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories,setRepositories] = useState([]);
+
+  //list repos
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, [] );
+
   async function handleAddRepository() {
     // TODO
   }
@@ -14,13 +25,17 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {
+          repositories.map(repository =>
+            <li key={repository.id}>
+              {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+              <button onClick={() => handleRemoveRepository(1)}>
+                Remover
+              </button>
+            </li>
+          )
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
